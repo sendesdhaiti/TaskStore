b64e3ea64dcc23f29b3f4b827d0320388685a59c
sendesdhaiti <sdhaiti.business@gmail.com>
2023-08-11 23:06:06 -0400

    Update to new styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BuyComponent } from './Comp/buy/buy.component';
+import { SellComponent } from './Comp/sell/sell.component';
+import { NavComponent } from './Comp/nav/nav.component';
+import { FooterComponent } from './Comp/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuyComponent,
+    SellComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
