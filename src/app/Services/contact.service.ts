64306c2387adeb80749c7faf420a27170fae3210
sendesdhaiti <sdhaiti.business@gmail.com>
2023-08-17 07:13:27 -0400

    udpate

--- src/app/Services/contact.service.ts
+++ src/app/Services/contact.service.ts
@@ -11,15 +11,16 @@ export class ContactService {
   constructor(private http:HttpClient) { }
   API = env.prod_api
   Contact_endpoint = "api/Contact/"
+  ACN_INTEREST_ENDPOINT = "send-email-message"
 
   SendEmailMessage(Contact:Contact, typeOfMessage:string){
     if(typeOfMessage == "BI"){
-      return this.http.post<boolean>(this.API + this.Contact_endpoint, Contact)
+      return this.http.post<boolean>(this.API + this.Contact_endpoint + this.ACN_INTEREST_ENDPOINT, Contact)
     }
     else if(typeOfMessage == "GI"){
-      return this.http.post<boolean>(this.API + this.Contact_endpoint, Contact)
+      return this.http.post<boolean>(this.API + this.Contact_endpoint + this.ACN_INTEREST_ENDPOINT, Contact)
     }else{
-      return this.http.post<boolean>(this.API + this.Contact_endpoint, Contact)
+      return this.http.post<boolean>(this.API + this.Contact_endpoint + this.ACN_INTEREST_ENDPOINT, Contact)
     }
   }
 }
